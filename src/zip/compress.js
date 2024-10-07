2ffad50f9324e1ab39ec2aa60cd22b851ca85df9
Stanislav M <minkostas@gmail.com>
2024-10-08 00:33:50 +0300

    5 part

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+  const fPath = path.join(process.cwd(), 'files', 'fileToCompress.txt');
+  const output = path.join(process.cwd(), 'files', 'archive.gz');
+
+  const rStream = fs.createReadStream(fPath);
+  const wStream = fs.createWriteStream(output);
+
+  const zipStream = zlib.createGzip();
+
+  rStream
+    .pipe(zipStream)
+    .pipe(wStream)
+    .on('finish', () => {
+      console.log('File compressed successfully');
+    });
+
+  rStream.on('error', (e) => {
+    console.error('Error reading file:', e.message);
+  });
+
+  wStream.on('error', (e) => {
+    console.error('Error writing file:', e.message);
+  });
+
+  zipStream.on('error', (e) => {
+    console.error('Error during compression:', e.message);
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    
+  const input = path.join(process.cwd(), 'files', 'archive.gz');
+  const output = path.join(process.cwd(), 'files', 'fileToCompress.txt');
+
+  const rStream = fs.createReadStream(input);
+  const wStream = fs.createWriteStream(output);
+
+  const gzStream = zlib.createGunzip();
+
+  rStream
+    .pipe(gzStream)
+    .pipe(wStream)
+    .on('finish', () => {
+      console.log('File decompressed successfully');
+    });
+
+  rStream.on('error', (e) => {
+    console.error('Error reading file:', e.message);
+  });
+
+  wStream.on('error', (e) => {
+    console.error('Error writing file:', e.message);
+  });
+
+  gzStream.on('error', (e) => {
+    console.error('Error during decompression:', e.message);
+  });
 };
 
 await decompress();
