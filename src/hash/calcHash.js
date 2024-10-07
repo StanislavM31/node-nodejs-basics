c6071dd2e6f8bac4d86d838065392411e8b96596
Stanislav M <minkostas@gmail.com>
2024-10-08 00:39:26 +0300

    calcHash.js part

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = path.join(process.cwd(), 'files', 'fileToCalculateHashFor.txt');
+  const readStream = fs.createReadStream(file);
+  const hash = crypto.createHash('sha256');
+
+  readStream.pipe(hash);
+  hash.setEncoding('hex');
+  hash.on('finish', () => {
+    console.log(hash.read());
+  });
+
+  readStream.on('error', (err) => {
+    console.error('Error reading file:', err.message);
+  });
 };
 
 await calculateHash();
