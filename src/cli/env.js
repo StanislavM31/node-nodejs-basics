1c3a8033f7c12f7812dd40722bd9775fdfb5b016
Stanislav M <minkostas@gmail.com>
2024-10-07 05:44:47 +0300

    2 part

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    process.env.RSS_version = '1.0.0';
+    process.env.RSS_host = 'localhost';
+    process.env.RSS_port = '8080';
 };
 
 parseEnv();
+    const envVars = process.env;
+    const rssVars = Object.entries(envVars)
+      .filter(([key]) => key.startsWith('RSS_'))
+      .map(([key, value]) => `${key}=${value}`)
+      .join('; ');
+    console.log(rssVars);
+    delete process.env.RSS_host;
+    delete process.env.RSS_port;
+    delete process.env.RSS_version;
+
+  parseEnv();
