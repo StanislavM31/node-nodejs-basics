f05137c7c03f13deabe111c1fa4be2f4c73db441
Stanislav M <minkostas@gmail.com>
2024-10-07 05:29:43 +0300

    1 part

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
-const copy = async () => {
-    // Write your code here 
+import path from 'path';
+import { promises as fs } from 'fs';
+
+const copy = async () => { 
+  const sourceFolderPath = path.join(process.cwd(), 'files');
+  const destinationFolderPath = path.join(process.cwd(), 'files_copy'); 
+
+  try {
+
+    const sourceFolderExists = await fs.stat(sourceFolderPath).catch(() => false);
+    if (!sourceFolderExists) {
+      throw new Error('FS operation failed: Source folder does not exist');
+    }
+
+
+    const destinationFolderExists = await fs.stat(destinationFolderPath).catch(() => false);
+    if (destinationFolderExists) {
+      throw new Error('FS operation failed: Destination folder already exists');
+    }
+
+
+    await fs.cp(sourceFolderPath, destinationFolderPath, { recursive: true });
+    console.log('Folder files_copy copied successfully');
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+import path from 'path';
 const rename = async () => {
-    // Write your code here 
+  const oldPath = path.join(process.cwd(), 'src', 'fs', 'files', 'wrongFilename.txt');
+  const newPath = path.join(process.cwd(), 'src', 'fs', 'files', 'properFilename.md');
+  try {
+    const oldFileExists = await fs.stat(oldPath).catch(() => false);
+    if (!oldFileExists) {
+      throw new Error('FS operation failed');
+    }
+    const newFileExists = await fs.stat(newPath).catch(() => false);
+    if (newFileExists) {
+      throw new Error('FS operation failed');
+    }
+    await fs.rename(oldPath, newPath);
+    console.log('File renamed successfully');
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await rename();
