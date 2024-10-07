ab6433b827485c9e9535641946c1ca469d953359
Stanislav M <minkostas@gmail.com>
2024-10-07 06:25:36 +0300

    4 part

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(process.cwd(), 'files', 'fileToRead.txt');
+
+  const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+  readStream.pipe(process.stdout);
+
+  readStream.on('error', (err) => {
+    console.error('Error reading file:', err.message);
+  });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    // введите слово, а программа его перевернет
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedData = chunk.toString().split('').reverse().join('') + '\n';
+      callback(null, reversedData);
+    }
+  });
+
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
+
+  reverseStream.on('error', (err) => {
+    console.error('Error during transformation:', err.message );
+  });
 };
 
 await transform();
+
+//node transform.js from this dir
+// to stop  "ctrl+c"
